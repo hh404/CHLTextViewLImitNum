272418e8bae0b1120c896ac6f65201b760030abb
CHL <975491309@qq.com>
2016-11-14 14:00:55 +0800

    限制UITextView的输入字数(支持中文和英文)以及提醒内容

--- CHLTextViewLimitNum/ViewController.m
+++ CHLTextViewLimitNum/ViewController.m
@@ -43,6 +43,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self.view addSubview:self.inputTextView];
 }
 
 
